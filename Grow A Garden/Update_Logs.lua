131be4cb138e007c0507c320b1a2a0bcfa087af4
WhatTheSavage <phoomphatrasha@gmail.com>
2025-06-02 16:42:36 +0700

    Add files via upload

---
+++ Grow A Garden/Update_Logs.lua
@@ -0,0 +1,49 @@
+_G.grow_a_garden_update_logs = {
+    {
+        Date = "2 / 6 / 25",
+        Content = {
+            ["Added"] = {
+                "Auto feed pets"
+            },
+            ["Remove"] = {
+
+            },
+            ["Fixed"] = {
+                "Auto sell fruit not working [ Mobile ]"
+            }
+        }
+    },
+    {
+        Date = "1 / 6 / 25",
+        Content = {
+            ["Added"] = {
+                "Auto sell pet",
+                "Auto Swarm event",
+                "Auto favorite"
+            },
+            ["Remove"] = {
+                "Auto Night event"
+            },
+            ["Fixed"] = {
+                "Harvest very laggy",
+                "Auto buy egg not working"
+            }
+        }
+    },
+    {
+        Date = "31 / 5 / 25",
+        Content = {
+            ["Added"] = {
+
+            },
+            ["Remove"] = {
+
+            },
+            ["Fixed"] = {
+                "Can't place egg",
+                "Can't plant seed",
+                "Can't harvest"
+            }
+        }
+    },
+}
