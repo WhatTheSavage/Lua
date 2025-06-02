eca3afe9e1600eb91628b8c1e8f65c55aa181361
WhatTheSavage <phoomphatrasha@gmail.com>
2025-06-02 17:02:26 +0700

    Update Update_Logs.lua

--- Grow A Garden/Update_Logs.lua
+++ Grow A Garden/Update_Logs.lua
@@ -1,49 +1,60 @@
-_G.grow_a_garden_update_logs = {
-    {
-        Date = "2 / 6 / 25",
-        Content = {
-            ["Added"] = {
-                "Auto feed pets"
-            },
-            ["Remove"] = {
+_G.script_setting = {
+    Owner = _G.Main.owner,
+    Name = "Grow a Garden",
+    Hub_Name = _G.Main.hub_name,
+    Discord_Link = _G.Main.discord_link,
+    Save_File = "/Grow_a_Garden.json",
 
-            },
-            ["Fixed"] = {
-                "Auto sell fruit not working [ Mobile ]"
-            }
-        }
-    },
-    {
-        Date = "1 / 6 / 25",
-        Content = {
-            ["Added"] = {
-                "Auto sell pet",
-                "Auto Swarm event",
-                "Auto favorite"
-            },
-            ["Remove"] = {
-                "Auto Night event"
-            },
-            ["Fixed"] = {
-                "Harvest very laggy",
-                "Auto buy egg not working"
-            }
-        }
-    },
-    {
-        Date = "31 / 5 / 25",
-        Content = {
-            ["Added"] = {
+    Update_Date = "",
+    Update_logs = {
+        {
+            Date = "2 / 6 / 25",
+            Content = {
+                ["Added"] = {
+                    "Auto feed pets"
+                },
+                ["Remove"] = {
 
-            },
-            ["Remove"] = {
-
-            },
-            ["Fixed"] = {
-                "Can't place egg",
-                "Can't plant seed",
-                "Can't harvest"
+                },
+                ["Fixed"] = {
+                    "Auto sell fruit not working [ Mobile ]"
+                }
             }
-        }
-    },
+        },
+        {
+            Date = "1 / 6 / 25",
+            Content = {
+                ["Added"] = {
+                    "Auto sell pet",
+                    "Auto Swarm event",
+                    "Auto favorite"
+                },
+                ["Remove"] = {
+                    "Auto Night event"
+                },
+                ["Fixed"] = {
+                    "Harvest very laggy",
+                    "Auto buy egg not working"
+                }
+            }
+        },
+        {
+            Date = "31 / 5 / 25",
+            Content = {
+                ["Added"] = {
+
+                },
+                ["Remove"] = {
+
+                },
+                ["Fixed"] = {
+                    "Can't place egg",
+                    "Can't plant seed",
+                    "Can't harvest"
+                }
+            }
+        },
+    }
 }
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/WhatTheSavage/Lua/refs/heads/main/Grow%20A%20Garden/Script.lua'))()
