1019eed2b762df0d83377e8334bf38684bd52a6e
WhatTheSavage <phoomphatrasha@gmail.com>
2025-06-02 17:04:54 +0700

    Update Update_Logs.lua

--- Grow A Garden/Update_Logs.lua
+++ Grow A Garden/Update_Logs.lua
@@ -5,7 +5,7 @@ _G.script_setting = {
     Discord_Link = _G.Main.discord_link,
     Save_File = "/Grow_a_Garden.json",
 
-    Update_Date = "",
+    Update_Date = "2 / 6 / 25",
     Update_logs = {
         {
             Date = "2 / 6 / 25",
